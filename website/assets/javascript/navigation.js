5236848a4aded790c128d321f944477df22c5a89
Binary <Kamified@DESKTOP-BP8OOFM>
2023-06-29 13:04:19 +0100

    Mobile navigation no longer bullies desktop navigation

--- website/assets/javascript/navigation.js
+++ website/assets/javascript/navigation.js
@@ -1,28 +1,50 @@
 $(document).ready(function () {
+    /*-----------------*/
+    /*   SCREEN SIZE   */
+    /*-----------------*/
+    const mediaQuery = window.matchMedia('(max-width: 700px)');
+
+    mediaQuery.addEventListener("change", handleDeviceChange);
+    handleDeviceChange(mediaQuery);
+
+    
+
     /*----------------*/
     /*   NAVIGATION   */
     /*----------------*/
     let show = 0;
-
     $('#nav_btn').click(function () {
-        if (show === 0) {
-            show = 1;
-            console.log("Showing Menu");
-            Show();
+
+        if ($('.navbar a').eq(1).css('display') == 'block') {
+            console.log("Hiding Menu - advanced");
+            hideMenu();
         } else {
-            show = 0;
-            console.log("Hiding Menu");
-            Hide();
+            console.log("Showing Menu - advanced");
+            showMenu();
         }
     });
 });
 
-
-function Show() {
-    $('.navbar a').css('display', 'block');
+function showMenu() {
+    $('.navbar a').show();
 }
 
-function Hide() {
-    $('.navbar a').css('display', 'none');
-    $('#nav_btn').css('display', 'block');
+function hideMenu() {
+    $('.navbar a').hide();
+    $('#nav_btn').show();
+}
+
+function clearNavCSS() {
+    $('.navbar a').removeAttr('style');
+    $('#nav_btn').removeAttr('style');
+}
+
+function handleDeviceChange(e) {
+    if (e.matches) {
+        console.log("Mobile");
+    }
+    else {
+        console.log("Bigger than mobile");
+        clearNavCSS();
+    }
 }
